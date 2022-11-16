f739827a19834237c757b0ed340d23e240966502
Wolf Pinz <wolfpinz@gmail.com>
2022-11-16 14:35:58 +0100

    refactored: review form on restaurant show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
